50eb80d18a5de49f69339500b5626085106ac8bc
Rafael Reis <rafaelfc.ads@gmail.com>
2019-03-02 10:13:16 -0300

    flight rest repository test

--- src/test/java/br/com/atech/test/flight/FlightRepositoryTest.java
+++ src/test/java/br/com/atech/test/flight/FlightRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -15,9 +18,24 @@ public class FlightRepositoryTest {
 	FlightRepository repository;
 	
 	@Test
+    public void findOne() {
+		Flight flight = this.repository.findOne(1L);
+        Assert.assertEquals("qwertyuiop", flight.getFlightCode());
+    }
+	
+	@Test
     public void list() {
-        Iterable<Flight> flights = this.repository.findAll();
-        Assert.assertEquals(2, flights.spliterator().estimateSize());
+		Pageable pageable = new PageRequest(0, 10);
+		Page<Flight> flights = this.repository.findAll(pageable);
+        Assert.assertEquals(2, flights.getTotalElements());
+    }
+	
+	@Test
+    public void findByFlightCodeContaining() {
+		Pageable pageable = new PageRequest(0, 10);
+		Page<Flight> flights = this.repository.findByFlightCodeContaining("qw", pageable);
+        Assert.assertEquals(1, flights.getTotalElements());
+        Assert.assertEquals("qwertyuiop", flights.iterator().next().getFlightCode());
     }
 	
 }
